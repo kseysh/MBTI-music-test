7a108745c78370f10f59555b1f8ad7984028645c
kseysh <69035864+kseysh@users.noreply.github.com>
2023-01-12 00:06:56 +0900

    Add files via upload

--- MBTI_Music_Test/js/start.js
+++ MBTI_Music_Test/js/start.js
@@ -1,105 +1,60 @@
 const main=document.querySelector("#main");
-const qna=document.querySelector("#qna");
-const result=document.querySelector("#result")
-const END_POINT=12;
-const select=[0,0,0,0,0,0,0,0,0,0,0,0];
-
-function calResult(){
-    
-    var result=select.indexOf(Math.max(...select));
-    return result;
-}
-function setResult(){
-    let point = calResult();
-    const resultName=document.querySelector('.resultName');
-    resultName.innerHTML=infoList[point].name;
-
-    var resultImg=document.createElement('img');
-    const imgDiv=document.querySelector('.resultImg');
-    var imgURL='img/image-'+point+'.png';
-    resultImg.src=imgURL;
-    resultImg.alt=point;
-    resultImg.classList.add('img-fluid')
-    imgDiv.appendChild(resultImg);
-
-    const resultDesc=document.querySelector('.resultDesc');
-    resultDesc.innerHTML=infoList[point].desc;
-}
+const questionSection=document.querySelector("#questionSection");
+const result=document.querySelector("#result");
+const ENDPOINT=12;
 function goResult(){
-    qna.style.WebkitAnimation="fadeOut 1s";
-    qna.style.animaition="fadeOut 1s";
-    setTimeout(() =>{
-        result.style.WebkitAnimation="fadeIn 1s";
-        result.style.animaition="fadeIn 1s";
-        setTimeout(()=>{
-            qna.style.display="none"
-            result.style.display="block"
-        },500)})
-        setResult();
+
+}
+
+function addAnswer(answerText,currentIdx){
+    if(currentIdx==ENDPOINT){
+        goResult();
     }
+    var answerBox=document.querySelector(".answerBox");
+    var button=document.createElement('button')
+    button.classList.add('answerList')
+    button.classList.add('my-1')
+    button.classList.add('my-3')
+    button.classList.add('py-3')
+      
 
+    answerBox.appendChild(button);
+    button.innerHTML=answerText;
 
-function addAnswer(answerText,qIdx,aIdx){
-    var ansBx = document.querySelector('.answerBox');
-    var ansBtn = document.createElement('button');
-    ansBx.appendChild(ansBtn);
-    ansBtn.classList.add('answerList');
-    ansBtn.classList.add('mb-3');
-    ansBtn.classList.add('py-3');
-    ansBtn.classList.add('mx-auto');
-
-    ansBtn.style.WebkitAnimation="fadeIn 1s";
-    ansBtn.style.animation="fadeIn 1s";
-
-    ansBtn.innerHTML=answerText;
-
-
-
-    ansBtn.addEventListener("click",function(){
-        var children = document.querySelectorAll('.answerList')
-        for(let i = 0;i<children.length;i++){
+    button.addEventListener("click",function(){
+        console.log('addEvent 호출')
+        var children=document.querySelectorAll('.answerList');
+        for (let i = 0;i<children.length;i++){
             children[i].disabled=true;
-            children[i].style.WebkitAnimation="fadeOut 0.5s";
-            children[i].style.animation="fadeOut 0.5s";
+            children[i].style.display='none'
         }
-        setTimeout(()=>{
-            var target = qnaList[qIdx].a[aIdx].type;
-            for(let i =0;i<target.length;i++){
-                select[target[i]]+=1;
-            }
-            for(let i =0;i<children.length;i++){
-                children[i].style.display="none"
-            }
-            goNext(++qIdx);
-        },450);
+        console.log('addEvent 끝날때의 currentIdx',currentIdx)
+        addQuestion(++currentIdx)
     }, false);
 
-}
-function goNext(qIdx){
-    if(qIdx===END_POINT){
-        goResult();
-        return;
-    }
-    var qBx = document.querySelector('.qBox');
-    qBx.innerHTML=qnaList[qIdx].q;
-    for(let i in qnaList[qIdx].a){ //console 돌렸을 때 i가 0,1,2가 나옴 파이썬과 다른 듯
-        addAnswer(qnaList[qIdx].a[i].answer,qIdx,i);
-    }
-    var status=document.querySelector('.statusBar')
-    status.style.width=(100/END_POINT)*(qIdx+1)+'%';
+
 }
 
-function begin(){
-    main.style.WebkitAnimation="fadeOut 1s";
-    main.style.animaition="fadeOut 1s";
-    setTimeout(() =>{
-        qna.style.WebkitAnimation="fadeIn 1s";
-        qna.style.animaition="fadeIn 1s";
-        setTimeout(()=>{
-            main.style.display="none"
-            qna.style.display="block"
-        },500);
-        let qIdx=0;
-        goNext(qIdx);
-    },500);
+function addQuestion(currentIdx){
+    var image = document.querySelector('.questionImage');
+
+    var question = document.querySelector(".question");
+    question.innerHTML=qnaList[currentIdx].q;
+    for(let i in qnaList[currentIdx].a){
+        console.log("addAnswer 호출",i);
+        addAnswer(qnaList[currentIdx].a[i].answer,currentIdx);
+    }
+    var status = document.querySelector('.statusBar');
+    status.style.width = (100/ENDPOINT) * (currentIdx+1) + '%';
+    var percent = document.querySelector('.percentage');
+    console.log("addQuestion 마지막 currentIdx : ",currentIdx)
+    percent.innerHTML=' '+ (1+currentIdx) + ':00 / ' + ENDPOINT +':00';
+
 }
+
+function goQuestionSection(){
+    main.style.display="none";
+    questionSection.style.display="block";
+    addQuestion(0);
+}
+
