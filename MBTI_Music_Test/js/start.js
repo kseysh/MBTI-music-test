1d8a9d4847ca6b24664e4c25f8d684daa5c400d5
kseysh <69035864+kseysh@users.noreply.github.com>
2023-02-03 23:02:07 +0900

    Add files via upload

--- MBTI_Music_Test/js/start.js
+++ MBTI_Music_Test/js/start.js
@@ -234,3 +234,15 @@ function goQuestionSection(){
     addQuestion(0);
 }
 
+let card = document.querySelector('.card');
+    card.addEventListener('click', click);
+
+
+function click(event) {
+  let elem = event.currentTarget;
+  if (elem.style.transform == "rotateY(180deg)") {
+            elem.style.transform = "rotateY(0deg)";
+        } else {
+            elem.style.transform = "rotateY(180deg)";
+        }
+    }
